1967eaf19c82acd7bb223b35ded9c35574108a5e
mohitinfluencer <mission20255@gmail.com>
2026-01-27 14:37:58 +0530

    debug: add health and not-found routes

---
+++ src/app/health/page.tsx
@@ -0,0 +1,8 @@
+export default function HealthPage() {
+    return (
+        <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+            <h1>System Status: Online</h1>
+            <p>If you can see this, the routing is working.</p>
+        </div>
+    );
+}
