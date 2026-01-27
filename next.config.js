85f2f0588c945b2ce6cd6fb95a85c7648b35083a
mohitinfluencer <mission20255@gmail.com>
2026-01-27 18:58:40 +0530

    Clean up next.config.js by removing properties
    Removed unused properties from nextConfig.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    serverExternalPackages: ["@prisma/client", "@remotion/bundler", "@remotion/renderer", "ffmpeg-static"],
-    output: "standalone",
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
-
-
 
 module.exports = nextConfig;
